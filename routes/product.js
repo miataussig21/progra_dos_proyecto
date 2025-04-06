a07a4c63f111d656e2f0683b1bdf06719d673d15
Mia Taussig <miataussig@Mias-MacBook-Air.local>
2025-04-06 17:35:59 -0300

    Recuperando node_modules que se habian borraro

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', productController.product)
-router.get('/', productController.productAdd)
+router.get('/add', productController.productAdd)
 
 module.exports = router;
