17b1c0055e73a99b8b2074befe05859d0a500c3c
milagrostschudy <mtschudy@udesa.edu.ar>
2025-04-07 14:23:25 -0300

    cree el login controller y más correcciones

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const loginController = require("../controllers/loginController")
+
+
 
 router.get("/", function(req,res){
     res.render();
