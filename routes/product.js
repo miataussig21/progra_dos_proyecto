d4ec16b8f3260afa9a3afea651f656d68cb2105e
Mia Taussig <miataussig@Mias-MacBook-Air.local>
2025-04-07 14:19:00 -0300

    Agregue un require que faltaba

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const productController = require("../controllers/productController")
 
 router.get('/', productController.product)
 router.get('/add', productController.productAdd)
