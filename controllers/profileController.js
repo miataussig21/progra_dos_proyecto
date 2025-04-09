71cbb3ddc68fe85644321d2820527fdff6851eee
sofisanma <ssanmarco@udesa.edu.ar>
2025-04-09 18:03:40 -0300

    corregido profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,7 +2,7 @@ const modulo = require("../db/modulo")
 
 const controlador = {
     profile: function(req,res){
-        res.render('profile', {Nombre: modulo.usuario.usuario, Foto:modulo.usuario.foto});
+        res.render('profile', {Nombre: modulo.usuario.usuario, Email: modulo.usuario.email, Foto:modulo.usuario.foto});
     }
 }
 
