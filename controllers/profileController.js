465c7e7bb5286db7fb0d7c49dcde5b453940cd5b
sofisanma <ssanmarco@udesa.edu.ar>
2025-04-09 17:43:03 -0300

    primer llamado al modulo

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,8 @@
+const modulo = require("../db/modulo")
+
 const controlador = {
     profile: function(req,res){
-        res.render('profile');
+        res.render('profile', {Nombre: modulo.usuario.usuario});
     }
 }
 
