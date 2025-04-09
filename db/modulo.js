959a2eb83b562b317de039194f5649ec5bb186ee
milagrostschudy <mtschudy@udesa.edu.ar>
2025-04-09 17:34:13 -0300

    creamos el modulo de datos

---
+++ db/modulo.js
@@ -0,0 +1,78 @@
+const modulo = {
+    usuario: {
+        email: "sofiasanmarcoa@gmailcom",
+        usuario: "SofiaSanmarco",
+        contrasenia: "198473403",
+        nacimiento: "08/02/2006",
+        documento: "47890765",
+        foto: "https://previews.123rf.com/images/maiborodin/maiborodin2302/maiborodin230201320/199371163-avatar-de-icono-de-cara-vac%C3%ADa-con-ilustraci%C3%B3n-de-vector-de-pelo-rojo.jpg",
+    },
+
+    productos: {
+        detalles: [{
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+        }, 
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            },
+        {
+        imagen: "../images/images/products/img-cafetera-mouilinex.jpg",
+        nombre: "Cafetera",
+        descripcion: "Una cafetera roja alalalalalla"
+            }
+        ],
+    comentarios: [
+        {
+            usuario: "Mia12345",
+            texto: "Alto producto, me facilito el cafe de todas las mañanas",
+            imagen: "../images/images/users/chica-pelo-corto"
+        }
+    ]
+
+
+}}
+
+
+
+
+
+module.exports = modulo; 
