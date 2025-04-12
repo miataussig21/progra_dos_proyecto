9d1bc626a96bf5872bb997c7c564b243458b44b4
Mia Taussig <miataussig@Mias-MacBook-Air.local>
2025-04-12 14:28:42 -0300

    Hice que el controller de product add requiera el modulo de DB para que funione

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,11 @@
+
+const modulo = require("../db/modulo")
 const controlador = {
   product: function(req, res, next) {
     res.render('product', { title: 'Express' });
   },
 	productAdd: function(req, res, next) {
-        res.render('product-add', { title: 'Express' });
+        res.render('product-add', {Nombre: modulo.usuario.usuario, Email: modulo.usuario.email, Foto:modulo.usuario.foto});
       },
 }
 
