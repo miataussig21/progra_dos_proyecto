1759920828a8d5096e2ffd7e77730ef7f6768fb5
sofisanma <ssanmarco@udesa.edu.ar>
2025-04-12 15:08:43 -0300

    productos agregados al modulo en db

--- db/modulo.js
+++ db/modulo.js
@@ -10,54 +10,54 @@ const modulo = {
 
     productos: {
         detalles: [{
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro1.jpg",
+        nombre: "Anna and the French kiss",
+        descripcion: "Anna and the French Kiss is the 2010 debut novel of Stephanie Perkins."
         }, 
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro2.jpg",
+        nombre: "The song of Achilles",
+        descripcion: "The Song of Achilles is a 2011 novel by American writer Madeline Miller."
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro3.jpg",
+        nombre: "Pride and Prejudice",
+        descripcion: "Pride and Prejudice is the second novel by English author Jane Austen, published in 1813."
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro4.jpg",
+        nombre: "Sense and Sensibility",
+        descripcion: "Sense and Sensibility is the first novel by the English author Jane Austen, published in 1811.."
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro5.jpg",
+        nombre: "Little Women",
+        descripcion: "Little Women is a coming-of-age novel written by American novelist Louisa May Alcott"
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro6.jpg",
+        nombre: "Romeo and Juliet",
+        descripcion: "CThe Tragedy of Romeo and Juliet, is a tragedy written by William Shakespeare early in his career."
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro7.jpg",
+        nombre: "Anne of Green Gables",
+        descripcion: "Anne of Green Gables is a 1908 novel by Canadian author Lucy Maud Montgomery"
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro8.jpg",
+        nombre: "Six of Crows",
+        descripcion: "Six of Crows is a fantasy novel written by the Israeli–American author Leigh Bardugo"
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro9.jpg",
+        nombre: "The Picture of Dorian Gray",
+        descripcion: "The Picture of Dorian Gray is an 1890 philosophical fiction and gothic horror novel by Irish writer Oscar Wilde"
             },
         {
-        imagen: "../images/images/products/img-cafetera-moulinex.jpg",
-        nombre: "Cafetera",
-        descripcion: "Cafetera Nestle Roja. Con más de 20 funciones distintas para hacer muchos tipos de cafe."
+        imagen: "../images/images/libro10.jpg",
+        nombre: "Emma",
+        descripcion: "Emma is a novel written by English author Jane Austen."
             }
         ],
     comentarios: [
