4ef91771f7ba0dceb40efb4d1738f6101993ce9e
sofisanma <ssanmarco@udesa.edu.ar>
2025-04-14 18:28:37 -0300

    corrigiendo errores

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const modulo = require("../db/modulo")
 
 const controlador = {
   product: function(req, res, next) {
-    res.render('product', {Producto: modulo.productos.detalles, Texto: modulo.productos.detalles ,Imagen: modulo.productos.detalles, Nombre: modulo.productos.comentarios, Email: modulo.usuario.email, foto:modulo.productos.comentarios, texto: modulo.productos.comentarios, usuario:modulo.productos.comentarios.usuario});
+    res.render('product', {Producto: modulo.productos.detalles, Email: modulo.usuario.email, comment:modulo.productos.comentarios});
   },
 	productAdd: function(req, res, next) {
         res.render('product-add', {Nombre: modulo.productos.comentarios[2].usuario});
