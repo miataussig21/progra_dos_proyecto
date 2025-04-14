4597024e842b29e425ad26005cdfa3dc5f78f66d
Mia Taussig <miataussig@Mias-MacBook-Air.local>
2025-04-14 18:36:45 -0300

    ultimos retoques

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,10 @@
 
 const modulo = require("../db/modulo")
 
+
 const controlador = {
   product: function(req, res, next) {
-    res.render('product', {Producto: modulo.productos.detalles, Email: modulo.usuario.email, comment:modulo.productos.comentarios});
+    res.render('product', {Producto: modulo.productos.detalles, comment:modulo.productos.comentarios});
   },
 	productAdd: function(req, res, next) {
         res.render('product-add', {Nombre: modulo.productos.comentarios[2].usuario});
