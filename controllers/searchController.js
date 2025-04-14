a92043dd2e198df3d03961f52c4005b9ce17dda9
sofisanma <ssanmarco@udesa.edu.ar>
2025-04-14 17:18:03 -0300

    hecho el for en search-results

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,7 +2,7 @@ const modulo = require("../db/modulo")
 
 const controlador = {
     search: function(req,res){
-        res.render('search-results', {Nombre: modulo.productos.detalles[3].nombre, Foto: modulo.productos.detalles[3].imagen});
+        res.render('search-results', {Nombre: modulo.productos.detalles, Imagen: modulo.productos.detalles, Texto: modulo.productos.detalles});
     }
 }
 
