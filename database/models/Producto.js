d18e8cc124796421b6a81fc68c39b85a8af5c984
milagrostschudy <mtschudy@udesa.edu.ar>
2025-05-26 16:26:02 -0300

    creo el modelo de Producto

---
+++ database/models/Producto.js
@@ -0,0 +1,52 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Producto";
+    let cols = {
+        id: {
+            autoIncrement : true,
+            primaryKey : true,
+            type : dataTypes.INTEGER
+        },
+        imagen:{
+            type: dataTypes.STRING
+        },
+        producto:{
+            type: dataTypes.STRING
+        },
+        descripcion:{
+            type: dataTypes.STRING
+        },
+        created_at: {
+	        type: dataTypes.DATE
+        },
+        updated_at: {
+	        type: dataTypes.DATE
+        },
+        usuarios_id: {
+            type: dataTypes.INTEGER
+        }
+
+    }
+
+        let config = {
+        tableName: "productos",
+        timestamps: false,
+        }
+
+        let Producto = sequelize.define(alias, cols, config);
+        Producto.associate = function(models){
+            Producto.hasMany(models.Usuario,{
+                as: "relacion_pu",
+                foreignKey: "usuarios_id"
+            })
+            return Producto
+        }
+
+         Producto.associate = function(models){
+            Producto.hasMany(models.Comentario, {
+            as: "relacion_pc",
+            foreignKey: "producto_id"
+            })
+        };
+        return Producto
+
+}
