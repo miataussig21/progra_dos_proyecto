5a48b5647adc192104c0f7f1217021776e69817e
sofisanma <ssanmarco@udesa.edu.ar>
2025-05-26 17:22:58 -0300

    punto 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,32 @@ const controlador = {
     login: function(req,res){
         res.render('login');
     }, 
+    process: function(req, res){
+        db.Usuario.findOne({
+            where: [{email: req.body.email}]
+        })
+        .then(function(usuario){
+            if(usuario != undefined){
+                res.redirect('/login')
+            } else {
+                res.send("ERROR")
+            }
+        })
+    },
+
+
     register: function(req,res){
         res.render('register');
     }, 
+    create: function(req, res){
+        db.Usuario.create({
+            name: req.body.usuario,
+            email: req.body.email,
+            contrasenia: req.body.contraseña,
+            fecha_de_nacimiento: req.body.fecha,
+        })
+
+    },
     profile: function(req,res){
         res.render('profile', {Nombre: modulo.usuario.usuario, Email: modulo.usuario.email, Foto:modulo.usuario.foto, Producto: modulo.productos.detalles, Imagen: modulo.productos.detalles, Texto: modulo.productos.detalles});
     }
