4d4a639791e06ea39dcfc95f4bc04e959d5aff6c
sofisanma <ssanmarco@udesa.edu.ar>
2025-05-28 12:58:00 -0300

    empezando search results

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,9 +1,16 @@
 const modulo = require("../db/modulo")
 const db = require("../database/models")
+const op = db.Sequelize.Op;
 
 const controlador = {
     search: function(req,res){
-        res.render('search-results', {Nombre: modulo.productos.detalles, Imagen: modulo.productos.detalles, Texto: modulo.productos.detalles});
+        db.Producto.findAll({
+            where: [{producto: {[op.like]: "%req.query.search_query%"}}]
+        })
+        .then(function(result){
+            db.Producto
+            res.render("search-results", {Muestra: result})
+        })
     }
 }
 
